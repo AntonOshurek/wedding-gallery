30c8b180117d70bd05d58eefa31ccb889b44ab10
Anton Ashurek <antonashurek@192.168.1.5>
2023-12-20 12:41:25 +0100

    delete routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,18 @@
 //router
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //pages
 import MainPage from '../pages/main-page';
 //variables
-import { AppRoute, rootBaseName } from '../variables/routing-variables';
+// import { AppRoute, rootBaseName } from '../variables/routing-variables';
 
 function App() {
   return (
-    <BrowserRouter basename={rootBaseName}>
-      <Routes>
-        <Route path={AppRoute.ROOT} element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter basename={rootBaseName}>
+    //   <Routes>
+    //     <Route path={AppRoute.ROOT} element={<MainPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <MainPage />
   );
 }
 
